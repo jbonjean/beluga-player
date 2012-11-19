4f66f025f9122e8eec71babf133d3b85883be4da
Julien Bonjean <julien@bonjean.info>
2012-11-19 17:47:33 -0500

    Release

--- src/main/java/info/bonjean/beluga/Main.java
+++ src/main/java/info/bonjean/beluga/Main.java
@@ -44,7 +44,10 @@ public class Main
 	public static void main(String[] args)
 	{
 		if (args.length == 1 && args[0].equals("-version"))
-			System.out.println("Beluga Player 0.1");
+		{
+			System.out.println("Beluga Player 0.3");
+			System.exit(0);
+		}
 
 		BelugaConfiguration.getInstance().load();
 		startUI();
