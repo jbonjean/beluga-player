dee1df93104ff1001697da404002d8ed1ad8102f
Julien Bonjean <julien@bonjean.info>
2013-03-03 03:25:19 -0500

    Clean up, refactoring

--- src/main/java/info/bonjean/beluga/PivotUI.java
+++ src/main/java/info/bonjean/beluga/PivotUI.java
@@ -1,44 +1,37 @@
 package info.bonjean.beluga;
 
-import java.awt.Dimension;
-import java.net.URL;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 import org.apache.pivot.beans.BXMLSerializer;
-import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
-import org.apache.pivot.util.Resources;
 import org.apache.pivot.wtk.Application;
-import org.apache.pivot.wtk.Button;
-import org.apache.pivot.wtk.ButtonPressListener;
 import org.apache.pivot.wtk.DesktopApplicationContext;
 import org.apache.pivot.wtk.Display;
-import org.apache.pivot.wtk.PushButton;
 import org.apache.pivot.wtk.Window;
 
-public class HelloBXML implements Application
+public class PivotUI implements Application
 {
 	private Window window = null;
 
 	public static void main(String[] args) throws BackingStoreException
 	{
 		Preferences preferences = Preferences.userNodeForPackage(DesktopApplicationContext.class);
-		preferences = preferences.node(HelloBXML.class.getName());
+		preferences = preferences.node(PivotUI.class.getName());
 		preferences.clear();
 		preferences.putInt("width", 600);
 		preferences.putInt("height", 400);
 		preferences.putBoolean("resizable", false);
 		preferences.flush();
 
-		DesktopApplicationContext.main(HelloBXML.class, new String[]{});
+		DesktopApplicationContext.main(PivotUI.class, new String[]{});
 	}
 	
 	@Override
 	public void startup(Display display, Map<String, String> properties) throws Exception
 	{
 		BXMLSerializer bxmlSerializer = new BXMLSerializer();
-		window = (Window) bxmlSerializer.readObject(HelloBXML.class, "hello.bxml");
+		window = (Window) bxmlSerializer.readObject(PivotUI.class, "main.bxml");
 		window.open(display);
 	}
 
--- src/main/java/info/bonjean/beluga/PivotWindow.java
+++ src/main/java/info/bonjean/beluga/PivotWindow.java
@@ -7,15 +7,19 @@ import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.util.Resources;
 import org.apache.pivot.wtk.Action;
+import org.apache.pivot.wtk.Button;
+import org.apache.pivot.wtk.ButtonPressListener;
 import org.apache.pivot.wtk.Component;
 import org.apache.pivot.wtk.Label;
+import org.apache.pivot.wtk.Meter;
+import org.apache.pivot.wtk.PushButton;
 import org.apache.pivot.wtk.Window;
 
-public class HelloWindow extends Window implements Bindable
+public class PivotWindow extends Window implements Bindable
 {
 	private final Window instance = this;
 
-	public HelloWindow()
+	public PivotWindow()
 	{
 		super();
 
@@ -37,7 +41,7 @@ public class HelloWindow extends Window implements Bindable
 				{
 					System.out.println("Refresh");
 					BXMLSerializer bxmlSerializer = new BXMLSerializer();
-					Window newWindow = (Window) bxmlSerializer.readObject(HelloBXML.class, "hello.bxml");
+					Window newWindow = (Window) bxmlSerializer.readObject(PivotUI.class, "main.bxml");
 					newWindow.open(getDisplay());
 					instance.close();
 				}
@@ -60,9 +64,29 @@ public class HelloWindow extends Window implements Bindable
 
 		Label artistName = (Label) namespace.get("artistName");
 		artistName.setText("by Artist Name");
+		
+		Label stationName = (Label) namespace.get("stationName");
+		stationName.setText("Radio Nova");
 
 		Label songTraits = (Label) namespace.get("songTraits");
 		songTraits
 				.setText(" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at iaculis erat. Vestibulum a sem a lacus vehicula aliquam. Integer vel porttitor turpis. Proin facilisis elementum neque id adipiscing. Morbi at lectus egestas nisi dignissim malesuada. Integer quis laoreet ipsum. Duis fringilla eleifend volutpat. Mauris tincidunt dapibus auctor. Integer nec massa elit. Ut eget turpis magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur dignissim fringilla risus, et suscipit quam blandit eget. \nMaecenas porta ornare sagittis. Pellentesque venenatis sapien sit amet elit lobortis rutrum in venenatis lorem. Suspendisse nec mi sit amet erat tincidunt pretium quis at leo. Suspendisse nec diam id neque accumsan consectetur malesuada dictum sem. Donec nec lobortis eros. Ut laoreet molestie accumsan. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi malesuada dolor lacinia arcu volutpat eu aliquam erat elementum. Mauris eget gravida elit. Ut tincidunt aliquet libero. Nulla fermentum nulla vel diam condimentum sodales. Vestibulum sed volutpat erat. Phasellus quis erat ac odio suscipit aliquam ac eget lorem.\nNam dapibus venenatis enim. Vestibulum eu ipsum non metus congue accumsan sit amet sed arcu. Integer vel orci molestie nisi iaculis imperdiet vitae et diam. Aliquam eu turpis ut magna consectetur pretium. Aliquam accumsan adipiscing purus, a gravida risus consectetur in. Nam luctus ultrices massa, consequat vulputate nibh vehicula luctus. Sed auctor diam nec lectus consequat et imperdiet justo pulvinar. Nulla metus sapien, mollis auctor dapibus id, volutpat pellentesque massa.");
+		
+		Label currentTime = (Label) namespace.get("currentTime");
+		currentTime.setText("0:59");
+		
+		Label totalTime = (Label) namespace.get("totalTime");
+		totalTime.setText("4:32");
+		
+		Meter progress = (Meter) namespace.get("progress");
+		progress.setPercentage(0.27);
+		
+		PushButton pushButton = (PushButton)namespace.get("exitButton");
+        pushButton.getButtonPressListeners().add(new ButtonPressListener() {
+            @Override
+            public void buttonPressed(Button button) {
+            	System.exit(0);
+            }
+        });
 	}
 }
