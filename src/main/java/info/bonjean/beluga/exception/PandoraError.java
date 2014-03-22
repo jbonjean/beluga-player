af0082862ac7d72a515d3215b913c61ce414227a
Julien Bonjean <julien@bonjean.info>
2014-03-22 16:08:46 -0400

    i18n: explicit error message keys

--- src/main/java/info/bonjean/beluga/exception/PandoraError.java
+++ src/main/java/info/bonjean/beluga/exception/PandoraError.java
@@ -30,14 +30,27 @@ import java.util.Map;
  */
 public enum PandoraError
 {
-	ERROR_CODE_UNKNOWN(-1), INTERNAL(0), MAINTENANCE_MODE(1), URL_PARAM_MISSING_METHOD(2), URL_PARAM_MISSING_AUTH_TOKEN(
-			3), URL_PARAM_MISSING_PARTNER_ID(4), URL_PARAM_MISSING_USER_ID(5), SECURE_PROTOCOL_REQUIRED(
-			6), CERTIFICATE_REQUIRED(7), PARAMETER_TYPE_MISMATCH(8), PARAMETER_MISSING(9), PARAMETER_VALUE_INVALID(
-			10), API_VERSION_NOT_SUPPORTED(11), LICENSING_RESTRICTIONS(12), INSUFFICIENT_CONNECTIVITY(
-			13), UNKNOWN_14(14), UNKNOWN_15(15), READ_ONLY_MODE(1000), INVALID_AUTH_TOKEN(1001), INVALID_CREDENTIALS(
-			1002), INVALID_STATION(1004), STATION_DOES_NOT_EXIST(1006), CALL_NOT_ALLOWED(1008), EMAIL_INVALID(
-			1011), PASSWORD_INVALID(1012), EMAIL_ALREADY_USED(1013), INVALID_ZIP(1024), INVALID_BIRTHYEAR(
-			1025), INVALID_BIRTHYEAR_AGE(1026), INVALID_GENDER(1027), OUT_OF_PLAYS(1039);
+	ERROR_CODE_UNKNOWN(-1, "pandoraErrorCodeUnknown"), INTERNAL(0, "pandoraInternal"), MAINTENANCE_MODE(
+			1, "pandoraMaintenanceMode"), URL_PARAM_MISSING_METHOD(2,
+			"pandoraUrlParamMissingMethod"), URL_PARAM_MISSING_AUTH_TOKEN(3,
+			"pandoraUrlParamMissingAuthToken"), URL_PARAM_MISSING_PARTNER_ID(4,
+			"pandoraUrlParamMissingPartnerId"), URL_PARAM_MISSING_USER_ID(5,
+			"pandoraUrlParamMissingUserId"), SECURE_PROTOCOL_REQUIRED(6,
+			"pandoraSecureProtocolRequired"), CERTIFICATE_REQUIRED(7, "pandoraCertificateRequired"), PARAMETER_TYPE_MISMATCH(
+			8, "pandoraParameterTypeMismatch"), PARAMETER_MISSING(9, "pandoraParameterMissing"), PARAMETER_VALUE_INVALID(
+			10, "pandoraParameterValueInvalid"), API_VERSION_NOT_SUPPORTED(11,
+			"pandoraApiVersionNotSupported"), LICENSING_RESTRICTIONS(12,
+			"pandoraLicensingRestrictions"), INSUFFICIENT_CONNECTIVITY(13,
+			"pandoraInsufficientConnectivity"), UNKNOWN_14(14, "pandoraUnknown14"), UNKNOWN_15(15,
+			"pandoraUnknown15"), READ_ONLY_MODE(1000, "pandoraReadOnlyMode"), INVALID_AUTH_TOKEN(
+			1001, "pandoraInvalidAuthToken"), INVALID_CREDENTIALS(1002, "pandoraInvalidCredentials"), INVALID_STATION(
+			1004, "pandoraInvalidStation"), STATION_DOES_NOT_EXIST(1006,
+			"pandoraStationDoesNotExist"), CALL_NOT_ALLOWED(1008, "pandoraCallNotAllowed"), EMAIL_INVALID(
+			1011, "pandoraEmailInvalid"), PASSWORD_INVALID(1012, "pandoraPasswordInvalid"), EMAIL_ALREADY_USED(
+			1013, "pandoraEmailAlreadyUsed"), INVALID_ZIP(1024, "pandoraInvalidZip"), INVALID_BIRTHYEAR(
+			1025, "pandoraInvalidBirthyear"), INVALID_BIRTHYEAR_AGE(1026,
+			"pandoraInvalidBirthyearAge"), INVALID_GENDER(1027, "pandoraInvalidGender"), OUT_OF_PLAYS(
+			1039, "pandoraOutOfPlays");
 
 	private static final Map<Long, PandoraError> lookup = new HashMap<Long, PandoraError>();
 
@@ -48,10 +61,12 @@ public enum PandoraError
 	}
 
 	private final long code;
+	private final String key;
 
-	private PandoraError(long code)
+	private PandoraError(long code, String key)
 	{
 		this.code = code;
+		this.key = key;
 	}
 
 	public long getCode()
@@ -59,21 +74,13 @@ public enum PandoraError
 		return code;
 	}
 
+	public String getMessageKey()
+	{
+		return key;
+	}
+
 	public static PandoraError get(long code)
 	{
 		return lookup.get(code);
-	}
-
-	public String getMessageKey()
-	{
-		StringBuffer sb = new StringBuffer("pandora");
-		String[] parts = name().split("_");
-		for (String part : parts)
-		{
-			sb.append(Character.toUpperCase(part.charAt(0)));
-			if (part.length() > 1)
-				sb.append(part.substring(1).toLowerCase());
-		}
-		return sb.toString();
 	}
 }
