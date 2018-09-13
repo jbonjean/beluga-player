7169ba77598cc943a1ab5653e093e40857426797
Julien Bonjean <julien@bonjean.info>
2018-09-12 20:41:21 -0400

    test: remove useless test code

--- src/main/java/info/bonjean/beluga/misc/ResourceBundleToResources.java
+++ src/main/java/info/bonjean/beluga/misc/ResourceBundleToResources.java
@@ -1,18 +1,18 @@
 /*
  * Copyright (C) 2012-2018 Julien Bonjean <julien@bonjean.info>
- * 
+ *
  * This file is part of Beluga Player.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
@@ -25,28 +25,23 @@ import java.io.PrintWriter;
 import java.util.Properties;
 
 /**
- * 
+ *
  * @author Julien Bonjean
- * 
+ *
  * I find it a lot easier to maintain a properties file.
- * 
+ *
  */
-public class ResourceBundleToResources
-{
+public class ResourceBundleToResources {
 	private static final String[] RESOURCES = { "messages" };
 
-	public static void main(String[] args)
-	{
-		if (args.length != 1)
-		{
+	public static void main(String[] args) {
+		if (args.length != 1) {
 			System.err.println("Invalid arguments");
 			System.exit(1);
 		}
 		String resourcesPath = args[0];
-		for (String resourceName : RESOURCES)
-		{
-			try
-			{
+		for (String resourceName : RESOURCES) {
+			try {
 				String inputFile = resourcesPath + resourceName + ".properties";
 				String outputFile = resourcesPath + resourceName + ".json";
 
@@ -57,8 +52,7 @@ public class ResourceBundleToResources
 				properties.load(input);
 
 				output.println("{");
-				for (Object key : properties.keySet())
-				{
+				for (Object key : properties.keySet()) {
 					StringBuffer sb = new StringBuffer();
 					sb.append(key);
 					sb.append(":\"");
@@ -71,9 +65,7 @@ public class ResourceBundleToResources
 				output.flush();
 				output.close();
 				input.close();
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
